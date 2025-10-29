fc36953d77379dd0c8a114af91fff6edeb22a988
yooooung3 <a88030754@gmail.com>
2025-10-29 18:15:16 +0900

    Update 02_이차함수.py

--- pages/02_이차함수.py
+++ pages/02_이차함수.py
@@ -9,18 +9,19 @@ a = st.slider("a값", -10.0, 10.0, 1.0, 0.1)
 h = st.slider("꼭짓점 x좌표 h", -20.0, 20.0, 0.0, 0.1)
 k = st.slider("꼭짓점 y좌표 k", -20.0, 20.0, 0.0, 0.1)
 
-# x값 생성
-x = np.linspace(-100, 100, 5000)  # 점을 충분히 많이 찍어 부드럽게
+# x값 생성 (h 중심으로 ±20 정도)
+x = np.linspace(h-20, h+20, 5000)
 y = a * (x - h)**2 + k
 
 # Plotly 그래프
 fig = go.Figure()
 fig.add_trace(go.Scatter(x=x, y=y, mode='lines', name=f'y={a}*(x-{h})^2 + {k}'))
 
-# 좌표축 표시
+# 좌표축 표시 및 y범위 조정
 fig.update_layout(
     xaxis=dict(title='x', zeroline=True, zerolinewidth=2, zerolinecolor='black'),
-    yaxis=dict(title='y', zeroline=True, zerolinewidth=2, zerolinecolor='black'),
+    yaxis=dict(title='y', zeroline=True, zerolinewidth=2, zerolinecolor='black',
+               range=[k-20, k+20]),  # k 주변으로 y축 범위
     title="이차함수 그래프",
     showlegend=True,
 )
